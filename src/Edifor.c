e73042c6a8a32149d4787d52a590af76caa4ed68
ferhatgec <ferhatgectao@gmail.com>
2020-11-15 19:47:06 +0300

    Fix: File open scenario.

--- src/Edifor.c
+++ src/Edifor.c
@@ -549,20 +549,21 @@ void editorOpen(char *filename) {
   editorSelectSyntaxHighlight();
 
   FILE *fp = fopen(filename, "r");
-  if (!fp) die("fopen");
-
-  char *line = NULL;
-  size_t linecap = 0;
-  ssize_t linelen;
-  while ((linelen = getline(&line, &linecap, fp)) != -1) {
-    while (linelen > 0 && (line[linelen - 1] == '\n' ||
-                           line[linelen - 1] == '\r'))
-      linelen--;
-    editorInsertRow(E.numrows, line, linelen);
+  if (fp) {
+  	char *line = NULL;
+  
+  	size_t linecap = 0;
+  	ssize_t linelen;
+  
+  	while ((linelen = getline(&line, &linecap, fp)) != -1) {
+    	while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) linelen--;
+    	editorInsertRow(E.numrows, line, linelen);
+  	}
+  
+  	free(line);
+  	fclose(fp);
+  	E.dirty = 0;
   }
-  free(line);
-  fclose(fp);
-  E.dirty = 0;
 }
 
 void editorSave() {
